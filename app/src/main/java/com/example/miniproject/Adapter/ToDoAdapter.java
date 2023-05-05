934661df9a2b6b1253074dd2b18b9f54c82c0dd3
ala-kad <kaddechiala@gmail.com>
2023-05-05 22:36:48 +0100

    Retrieve tasks from firestore DB

---
+++ app/src/main/java/com/example/miniproject/Adapter/ToDoAdapter.java
@@ -0,0 +1,81 @@
+package com.example.miniproject.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.miniproject.MainActivity;
+import com.example.miniproject.Model.ToDoModel;
+import com.example.miniproject.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder>{
+
+    private List<ToDoModel> toDoList;
+    private MainActivity activity;
+    private FirebaseFirestore firestore;
+
+    //Class constructor & initalizing propreties
+    public ToDoAdapter(MainActivity mainActivity, List<ToDoModel> toDoList){
+        this.toDoList = toDoList;
+        activity =mainActivity;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.each_task, parent, false);
+        firestore = FirebaseFirestore.getInstance();
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        ToDoModel toDoModel = toDoList.get(position);
+        holder.mCheckBox.setText(toDoModel.getTask());
+        holder.mDueDate.setText("Due on" + toDoModel.getDue());
+        holder.mCheckBox.setChecked(toBoolean(toDoModel.getStatus()));
+
+        holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    firestore.collection("task").document(toDoModel.TaskId).update("status",0);
+                }else {
+                    firestore.collection("task").document(toDoModel.TaskId).update("status",1);
+                }
+            }
+        });
+    }
+    //converting status proprety to boolean if 0 false, true if 1
+    private boolean toBoolean (int status){
+        return status !=0 ;
+    }
+
+    //return the todolist number of items
+    @Override
+    public int getItemCount() {
+        return toDoList.size();
+    }
+
+    public class  MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView mDueDate;
+        CheckBox mCheckBox;
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            mDueDate = itemView.findViewById(R.id.due_date_tv);
+            mCheckBox = itemView.findViewById(R.id.mycheckbox);
+        }
+    }
+
+}
--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.miniproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,13 +8,29 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.miniproject.Adapter.ToDoAdapter;
+import com.example.miniproject.Model.ToDoModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private RecyclerView recyclerView;
     private FloatingActionButton Fabtn;
+    private FirebaseFirestore firestore;
+    private ToDoAdapter adapter;
+    private List<ToDoModel> toDoModelList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
         Fabtn = findViewById(R.id.floatingActionButton);
-
+        firestore = FirebaseFirestore.getInstance();
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
@@ -29,6 +46,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+        toDoModelList = new ArrayList<>();
+        adapter = new ToDoAdapter(MainActivity.this, toDoModelList);
+
+        recyclerView.setAdapter(adapter);
+        showData();
+    }
+
+    private void showData(){
+        firestore.collection("task").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                for (DocumentChange documentChange : value.getDocumentChanges()){
+                    if(documentChange.getType() == DocumentChange.Type.ADDED){
+                        String id = documentChange.getDocument().getId();
+                        ToDoModel toDoModel = documentChange.getDocument().toObject(ToDoModel.class).withId(id);
+                        toDoModelList.add(toDoModel);
+                        //Notify adapter that data in the toDoModel list has changed
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                Collections.reverse(toDoModelList);
             }
         });
     }
