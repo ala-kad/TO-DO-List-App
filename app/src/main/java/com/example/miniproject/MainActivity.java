8bfb3f2a890b89452a3c5f1b90071cbf5b427e5c
ala-kad <kaddechiala@gmail.com>
2023-05-07 11:38:45 +0100

    Fixing Design + Logout Using fireBaseAuth

--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -7,12 +7,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.miniproject.Adapter.ToDoAdapter;
 import com.example.miniproject.Model.ToDoModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -36,24 +39,44 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
     private List<ToDoModel> toDoModelList;
     private  Query query;
     private ListenerRegistration listenerRegistration;
+    private Button logoutBtn;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Binding activity views to class attributes
         recyclerView = findViewById(R.id.recyclerView);
         Fabtn = findViewById(R.id.floatingActionButton);
         firestore = FirebaseFirestore.getInstance();
+        logoutBtn = findViewById(R.id.logoutBtn);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Logout Business Logic
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                //Navigating user to Login Activity after SigningOut
+                startActivity(new Intent(MainActivity.this, LoginActivty.class));
+                finish();
+            }
+        });
+
+        //setting fixed size to recycler view
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
+        //Add new task button click
         Fabtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
             }
         });
+
+        //Adapting recycler view
         toDoModelList = new ArrayList<>();
         adapter = new ToDoAdapter(MainActivity.this, toDoModelList);
 
@@ -63,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
         recyclerView.setAdapter(adapter);
     }
 
+    //Getting all tasks and rendering them
     private void showData(){
        query = firestore.collection("task").orderBy("time", Query.Direction.DESCENDING);
        listenerRegistration = query.addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -83,6 +107,7 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
         });
     }
 
+    //clearing the taskList after closing the model
     @Override
     public void onDialogCLose(DialogInterface dialogInterface) {
         toDoModelList.clear();
